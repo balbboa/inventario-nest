151bfeb7b9e93f04b2705550ff1a9a99bc0e895c
balbboa <abmm_arthur@hotmail.com>
2023-02-09 11:46:15 -0300

    Feat: (servicetype, supplier, service) controller

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 - yarn
 - prisma generate
 - sudo docker run --name postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -p 5432:5432 -d postgres (create)
-- sudo docker run -e POSTGRES_PASSWORD=postgres -p 5432:5432 postgres (run)
+- sudo docker ps -a ---|> sudo docker start postgres (run)
 - prisma migrate dev
 - yarn start:dev
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GroupsModule } from './api/v1/groups/groups.module';
+import { ImmobilesModule } from './api/v1/immobile/immobiles.module';
 import { ItemsModule } from './api/v1/items/items.module';
 import { ManufacturersModule } from './api/v1/manufacturers/manufacturers.module';
 import { ModelsModule } from './api/v1/models/models.module';
 import { OpmsModule } from './api/v1/opms/opms.module';
+import { ServicesModule } from './api/v1/services/services.module';
+import { ServiceTypesModule } from './api/v1/serviceTypes/serviceTypes.module';
 import { StatusModule } from './api/v1/status/status.module';
+import { SuppliersModule } from './api/v1/suppliers/suppliers.module';
 
 @Module({
   imports: [
@@ -13,7 +17,11 @@ import { StatusModule } from './api/v1/status/status.module';
     OpmsModule,
     GroupsModule,
     ItemsModule,
-    StatusModule
+    StatusModule,
+    ImmobilesModule,
+    ServicesModule,
+    SuppliersModule,
+    ServiceTypesModule
   ],
   controllers: [],
   providers: []
